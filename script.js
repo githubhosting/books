0ad018f4db45cedd9e9156ab193da59f4d500e37
Shravan Revanna <94772842+githubhosting@users.noreply.github.com>
2022-04-12 03:16:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,54 @@
-(function() {
-
-
-}).call(this);
+(function () {}.call(this));
 
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiPGFub255bW91cz4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7O0FBQUEiLCJzb3VyY2VzQ29udGVudCI6WyIiXX0=
 //# sourceURL=coffeescript
 
+var btn = $("button1");
 
-var btn = $('button1');
-
-$(window).scroll(function() {
+$(window).scroll(function () {
   if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
+    btn.addClass("show");
   } else {
-    btn.removeClass('show');
+    btn.removeClass("show");
   }
 });
 
-btn.on('click', function(e) {
+btn.on("click", function (e) {
   e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
+  $("html, body").animate({ scrollTop: 0 }, "300");
 });
 
-let button = document.querySelector('.button-3')
+let button = document.querySelector(".button-3");
 
-button.addEventListener('click', ()=>{
-  document.documentElement.classList.toggle('dark-mode')
-})
+button.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark-mode");
+});
 
-function change1() // no ';' here
-{
-    var elem = document.getElementById("button-3");
-    if (elem.value=="Dark mode") elem.value = "Light mode";
-    else elem.value = "Dark mode";
+function change1() {
+  // no ';' here
+  var elem = document.getElementById("button-3");
+  if (elem.value == "Dark mode") elem.value = "Light mode";
+  else elem.value = "Dark mode";
 }
 
-function change() // no ';' here
-{
-    var elems = document.getElementById("Myid1");
-    if (elems.value=="Show Countdown") elems.value = "Hide Countdown";
-    else elems.value = "Show Countdown";
+function change() {
+  // no ';' here
+  var elems = document.getElementById("Myid1");
+  if (elems.value == "Show Countdown") elems.value = "Hide Countdown";
+  else elems.value = "Show Countdown";
 }
 
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 1000,
+  reset: true,
+});
+
+sr.reveal(".container", { delay: 50 });
+sr.reveal(".btn", { distance: "20px", delay: 50, interval: 90 });
+
+sr.reveal(".card", { delay: 50, interval: 100, reset: false });
+sr.reveal(".quote", { distance: "10px", delay: 100 });
+sr.reveal(".notice", { distance: "10px", delay: 150 });
